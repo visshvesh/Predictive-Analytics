2e1725a3ff16526330f79f52fe2ee70ca15b1576
visshvesh <32333984+visshvesh@users.noreply.github.com>
2017-12-22 15:02:01 +0530

    Add files via upload

---
+++ vish22nd/WithoutLookupClient2.py
@@ -0,0 +1,69 @@
+# has data.shape at line 50
+
+from grpc.beta import implementations
+import numpy
+import tensorflow as tf
+from datetime import datetime
+#from tensorflow_serving_client import  predict_pb2
+from tensorflow_serving.apis import predict_pb2
+from tensorflow_serving.apis import prediction_service_pb2
+
+tf.app.flags.DEFINE_string('server', 'localhost:9000', 'PredictionService host:port')
+tf.app.flags.DEFINE_string('errorcode', '110', 'path to errorcode')
+FLAGS = tf.app.flags.FLAGS
+
+
+# def do_inference(hostport):
+def do_inference(hostport):
+    """Tests PredictionService with concurrent requests.
+    Args:
+    hostport: Host:port address of the Prediction Service.
+    Returns:
+    pred values, ground truth label
+    """
+    # create connection
+    host, port = hostport.split(':')
+    channel = implementations.insecure_channel(host, int(port))
+    stub = prediction_service_pb2.beta_create_PredictionService_stub(channel)
+    print("Connection is Done*************************")
+    # initialize a request
+    request = predict_pb2.PredictRequest()
+    request.model_spec.name = 'saved1_model'
+    request.model_spec.signature_name = 'prediction'
+    #data = FLAGS.errorcode
+    #print(data)
+    data = '103'
+
+
+# Randomly generate some test data
+# temp_data = numpy.random.randn(10, 3).astype(numpy.float32)
+# data, label = temp_data, numpy.sum(temp_data * numpy.array([1, 2, 3]).astype(numpy.float32), 1)
+# request.inputs['input'].CopyFrom(
+#     tf.contrib.util.make_tensor_proto(data, shape=data.shape))
+
+
+
+# # Randomly generate some test data
+# temp_data = numpy.random.randn(10, 3).astype(numpy.float32)
+# data, label = temp_data, numpy.sum(temp_data * numpy.array([1, 2, 3]).astype(numpy.float32), 1)
+    request.inputs['input'].CopyFrom(
+        tf.contrib.util.make_tensor_proto(data, shape=data.shape))
+
+    # predict
+    result = stub.Predict(request, 30.0)  # 5 seconds
+    print(result)
+    return result
+
+
+def main(_):
+    if not FLAGS.server:
+        print('please specify server host:port')
+        return
+
+    result = do_inference(FLAGS.server)
+    print(result)
+    # print('Actual label is: ', label)
+
+
+if __name__ == '__main__':
+    tf.app.run()
